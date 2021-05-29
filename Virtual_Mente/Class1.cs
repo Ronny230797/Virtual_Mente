39cdc4476139f7510b33b0080f1c1b8138b194f1
mariaPri <priscicastro21@hotmail.com>
2021-05-29 10:13:38 -0600

    Creación clase
    Creación clase 1

---
+++ Virtual_Mente/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Virtual_Mente
+{
+    public class Class1
+    {
+        private int pri;
+    }
+}
