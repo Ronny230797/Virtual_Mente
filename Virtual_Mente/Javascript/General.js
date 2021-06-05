447a30fa402d07ffb0a785519a9e868decc6d33e
Ronny Alvarez <ronny@grupomundial.cr>
2021-06-05 07:45:48 -0600

    Changes by Ronny

---
+++ Virtual_Mente/Javascript/General.js
@@ -0,0 +1,13 @@
+﻿//Local Storage
+//To Help: https://blog.logrocket.com/localstorage-javascript-complete-guide/
+let saveLocalStorage = (key, value) => {
+    window.localStorage.setItem(`${key}`, `${value}`);
+}
+
+let getLocalStorage = (key) => {
+    JSON.parse(window.localStorage.getItem(`${key}`));
+}
+
+let clearLocalStorage = () => {
+    window.localStorage.clear();
+}
