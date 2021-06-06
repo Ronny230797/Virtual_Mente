7399f3e02ce2f2a1441a6ef6a25cb88985faed85
Pedro <pldiaz24@gmail.com>
2021-06-05 19:18:10 -0600

    Conexión DB
    Añadi un nuevo contolador para ls conexiónDB

---
+++ Virtual_Mente/Controllers/ConnectionDBController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Virtual_Mente.Controllers
+{
+    public class ConnectionDBController : Controller
+    {
+        String bondConnection = "Data source=LAPTOP-PHDA6TV8\\MSSQLSERVER19;Initial Catalog=VirtualMente; Integrated Security=True";
+        public SqlConnection connection = new SqlConnection ();
+
+
+        public ConnectionDBController()
+        {
+            connection.ConnectionString = bondConnection;
+        }
+
+        public void openConnection()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection open");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Failed to open "+ex.Message);
+            }
+
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
