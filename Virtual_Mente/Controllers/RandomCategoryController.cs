0ea0359b5d76c0d5ce610c6581b0ecd0943cfa37
Maria Jesus <maria.rodriguezbenavides@ucr.ac.cr>
2021-06-10 20:52:27 -0600

    Cambios MAria

---
+++ Virtual_Mente/Controllers/RandomCategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Virtual_Mente.Controllers
+{
+    public class RandomCategoryController : Controller
+    {
+        // GET: RandomCategory
+
+        public ActionResult test()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
