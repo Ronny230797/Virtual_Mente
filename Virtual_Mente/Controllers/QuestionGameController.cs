650ed05d11f0c09326f3953008953eb74891cc98
JennerCR29 <jenner2905@gmail.com>
2021-06-19 09:05:56 -0600

    Muestra de preguntas y respuestas

--- Virtual_Mente/Controllers/QuestionGameController.cs
+++ Virtual_Mente/Controllers/QuestionGameController.cs
@@ -9,12 +9,21 @@ namespace Virtual_Mente.Controllers
 {
     public class QuestionGameController : Controller
     {
-        
+        int idPregunta;
+        string opcion1 = "";
+        string opcion2 = "";
+        string opcion3 = "";
+        string opcion4 = "";
 
         public ActionResult Main(string Department = "")
         {
             ViewBag.Department = Department;
             ViewBag.Pregunta = traerPreguntas(Department);
+            traerOpciones();
+            ViewBag.Opcion1 = opcion1;
+            ViewBag.Opcion2 = opcion2;
+            ViewBag.Opcion3 = opcion3;
+            ViewBag.Opcion4 = opcion4;
             return View();
         }
 
@@ -22,8 +31,14 @@ namespace Virtual_Mente.Controllers
         {
             ViewBag.Dep = Depart;
             ViewBag.Pregunta = traerPreguntas(Depart);
+            traerOpciones();
+            ViewBag.Opcion1 = opcion1;
+            ViewBag.Opcion2 = opcion2;
+            ViewBag.Opcion3 = opcion3;
+            ViewBag.Opcion4 = opcion4;
             return PartialView();
         }
+
         // DIVIDIR ESTE METODO
         public string traerPreguntas(string Department = "")
         {
@@ -38,7 +53,7 @@ namespace Virtual_Mente.Controllers
             List<Pregunta> list = db.PREGUNTA.Select(x => new Pregunta
             {
                 id = x.IDpregunta,
-                description = x.DescPregunta,
+                descripcion = x.DescPregunta,
                 idTipoJuego = x.IDTipoJuegoFK,
                 idCategoria = x.IDCategoriaFK
             }).ToList();
@@ -54,17 +69,56 @@ namespace Virtual_Mente.Controllers
             Random r1 = new Random();
 
             int aleatorio = r1.Next(listaPreguntasCat.Count);
-            string txt = listaPreguntasCat[aleatorio].description;
+            string txt = listaPreguntasCat[aleatorio].descripcion;
+            idPregunta = listaPreguntasCat[aleatorio].id;
 
             return txt;
+        }
+
+        public void traerOpciones()
+        {
+            VirtualMenteEntities db = new VirtualMenteEntities();
+            List<Opcion> listaOpcionesXPregunta = new List<Opcion>();
+
+
+            List<Opcion> listaOpciones = db.OPCION.Select(x => new Opcion
+            {
+                idOpcion = x.IDOpcion,
+                descripcion = x.DescOpcion,
+                idPreguntaFK = x.IDPeguntaFK,
+                idRespuestaCorrectaFK = x.IDRepuestaCorrectaFK
+            }).ToList();
+
+
+            foreach (Opcion var in listaOpciones)
+            {
+                if (var.idPreguntaFK == idPregunta)
+                {
+                    listaOpcionesXPregunta.Add(var);
+                }
+            }
+
+            opcion1 = listaOpcionesXPregunta[0].descripcion;
+            opcion2 = listaOpcionesXPregunta[1].descripcion;
+            opcion3 = listaOpcionesXPregunta[2].descripcion;
+            opcion4 = listaOpcionesXPregunta[3].descripcion;
+
         }
 
         public class Pregunta
         {
             public int id { set; get; }
-            public string description { set; get; }
+            public string descripcion { set; get; }
             public int idTipoJuego { set; get; }
             public int idCategoria { set; get; }
+        }
+
+        public class Opcion
+        {
+            public int idOpcion { set; get; }
+            public string descripcion { set; get; }
+            public int idPreguntaFK { set; get; }
+            public int idRespuestaCorrectaFK { set; get; }
         }
 
     }
