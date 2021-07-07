5ce6dc3f401d1fab545403eb9f1854cb716153bc
Maria Jesus <maria.rodriguezbenavides@ucr.ac.cr>
2021-07-06 18:37:53 -0600

    cambios en structure question, variable local storage

---
+++ Virtual_Mente/Controllers/correctAnwserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Virtual_Mente.Controllers
+{
+    public class correctAnwserController : Controller
+    {
+        // GET: correctAnwser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
