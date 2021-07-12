4dd8986e387f987ccff14e42b4cb1e6aed7da93e
JennerCR29 <jenner2905@gmail.com>
2021-07-12 16:05:00 -0600

    arreg

--- Virtual_Mente/App_Start/RouteConfig.cs
+++ Virtual_Mente/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Virtual_Mente
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ChooseGame", action = "Main", id = UrlParameter.Optional }
             );
         }
     }
